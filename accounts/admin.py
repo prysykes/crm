6a8788ed2a1d073ec27bcf233469e8165868cfc1
Emeka Duru <emisykes2000@yahoo.com>
2020-09-15 21:10:32 +0100

    initial commit

---
+++ accounts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from . models import Customer, Product, Order, Tag
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Tag, TagAdmin)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+admin.site.register(Product, ProductAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'product', 'status']
+
+admin.site.register(Order, OrderAdmin)
+# Register your models here.
---
+++ accounts/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.DecimalField(max_digits=1000, decimal_places=2)
+    category = models.CharField(max_length=200, choices=CATEGORY, null=True)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name + " - " + str(self.price)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered')
+    )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=200, choices=STATUS, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return str(self.customer) + str(self.product) + str(self.status)
